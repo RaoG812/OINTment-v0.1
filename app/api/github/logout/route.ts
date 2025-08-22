f89b3973bc43fcb317802ecbdeaa51f215469f3b
RaoG812 <140965516+RaoG812@users.noreply.github.com>
2025-08-22 18:46:51 +0700

    fix logout redirect

--- app/api/github/logout/route.ts
+++ app/api/github/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
-  const res = NextResponse.redirect('/')
+  const res = NextResponse.redirect(new URL('/', req.url))
   res.cookies.set('github_token', '', {
     httpOnly: true,
     secure: true,
